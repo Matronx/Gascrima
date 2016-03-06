a7a079d66d04a5279c4044248ff635b149c57951
Mathias <Mathias@Mathias-PC>
2016-03-06 19:12:36 -0300

    Test -- 01

---
+++ source/MWindow.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   MWindow.cpp
+ * Author: Mathias
+ * 
+ * Created on 4 de marzo de 2016, 23:30
+ */
+
+#include "MWindow.h"
+
+MWindow::MWindow() {
+    printf("Hola Mundo");
+}
+
+MWindow::MWindow(const MWindow& orig) {
+}
+
+MWindow::~MWindow() {
+}
+
