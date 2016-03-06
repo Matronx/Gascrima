ed077bf2f9d113f7b92db8cdf6be7e46c80162a5
Mathias <Mathias@Mathias-PC>
2016-03-06 12:55:37 -0300

    Test -- 01

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: Mathias
+ *
+ * Created on 4 de marzo de 2016, 23:22
+ */
+
+#include <cstdlib>
+#include "include/MWindow.h"
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    MWindow     *Ventana = new MWindow();
+    return 0;
+}
+
